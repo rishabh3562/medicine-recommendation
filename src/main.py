d2d2b081d929863ba393476aa7fb9ac490039619
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-03-27 20:42:05 +0530

    deploy : fix

--- src/main.py
+++ src/main.py
@@ -130,7 +130,5 @@ def developer():
 def blog():
     return render_template("blog.html")
 
-
-if __name__ == '__main__':
-
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
